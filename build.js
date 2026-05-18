68e2c0a1842b03eaa0dded363f449b85f86e3b98
isadora-ui <isadora@thegrint.com>
2026-05-17 23:15:02 -0300

    Delete build.js

--- build.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const timestamp = Date.now();
-
-let sw = fs.readFileSync('sw.js', 'utf8');
-sw = sw.replace('__DEPLOY_TIME__', timestamp);
-fs.writeFileSync('sw.js', sw);
-
-console.log(`✅ sw.js atualizado com cache version: thegrint-pool-${timestamp}`);
